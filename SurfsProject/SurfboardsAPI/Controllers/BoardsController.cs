c09ff400d25b7fcf248af36153bf46a2af339e77
RasmusEnevoldsen <112504682+RasmusEnevoldsen@users.noreply.github.com>
2023-10-01 18:04:36 +0200

    Started working on API
    Set up Controller, Model. Is working atm.

---
+++ SurfsProject/SurfboardsAPI/Controllers/BoardsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SurfboardsAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BoardsController : ControllerBase
+    {
+        [HttpGet]
+        public string GetBoards()
+        {
+            return "OK";
+        }
+    }
+}
---
+++ SurfsProject/SurfboardsAPI/Models/Surfboards.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace SurfboardsAPI.Models
+{
+    public class Surfboards
+    {
+        public class Surfboard
+        {
+            public int Id { get; set; }
+
+            public string Name { get; set; }
+
+            [Column(TypeName = "decimal(18, 2)")]
+            public decimal Length { get; set; }
+
+            [Column(TypeName = "decimal(18, 2)")]
+            public decimal Width { get; set; }
+
+            [Column(TypeName = "decimal(18, 2)")]
+            public decimal Thickness { get; set; }
+
+            [Column(TypeName = "decimal(18, 2)")]
+            public decimal Volume { get; set; }
+
+            public string Type { get; set; }
+
+            [Column(TypeName = "decimal(18, 2)")]
+            public decimal Price { get; set; }
+
+            public string? Equipment { get; set; }
+            [ForeignKey("dbo.AspNetUsers")]
+            public int? Rentee { get; set; }
+
+            [Timestamp]
+            public byte[] RowVersion { get; set; }
+        }
+    }
+}

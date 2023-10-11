43ce716ffe65a002ddfb09485db05d67e30275dd
Mapendo Kelly <make48949@edu.ucl.dk>
2023-10-11 20:42:46 +0200

    Added SurfboardTests for the Model folder.

---
+++ SurfsProject/SurfsProjectTest/SurfboardTest.cs
@@ -0,0 +1,29 @@
+﻿using SurfsProject.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using SurfsProject.Controllers;
+using SurfsProject.Models;
+using System.Diagnostics;
+
+namespace SurfsProjectTest
+{
+    [TestClass]
+    public class SurfboardTests
+    {
+        [TestMethod]
+        public void TestSurfboardLength() 
+        {
+            // Arrange
+            var surfboard = new Surfboard { Length = 6.0m };
+
+            // Act
+            var result = surfboard.Length;
+
+            // Assert
+            Assert.AreEqual(6.0m, result);
+        }
+
+    }
+}

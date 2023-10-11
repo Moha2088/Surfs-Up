b13c3dcc5d3cac6291211359eda17bc8471602a6
Mapendo Kelly <make48949@edu.ucl.dk>
2023-10-11 21:05:39 +0200

    More Tests for the Surboard class in Models.

--- SurfsProject/SurfsProjectTest/SurfboardTest.cs
+++ SurfsProject/SurfsProjectTest/SurfboardTest.cs
@@ -25,5 +25,59 @@ namespace SurfsProjectTest
             Assert.AreEqual(6.0m, result);
         }
 
+        [TestMethod]
+        public void TestSurfboardWidth() 
+        {
+            // Arrange 
+            var surfboard = new Surfboard { Width = 30 };
+
+            // Act
+            var result = surfboard.Width;
+
+            // Assert
+            Assert.AreEqual(30, result);
+        
+        }
+
+        [TestMethod]
+        public void TestSurfboardThickness() 
+        {
+            // Arrange
+            var surfboard = new Surfboard { Thickness = 2.75m};
+
+            // Act
+            var result = surfboard.Thickness;
+
+            // Assert
+            Assert.AreEqual(2.75m, result);
+        }
+
+
+        [TestMethod]
+        public void TestSurfboardVolume() 
+        {
+            // Arrange
+            var surfboard = new Surfboard { Volume = 30.2m };
+
+            // Act
+            var result = surfboard.Volume;
+
+            // Assert
+            Assert.AreEqual(30.2m, result);
+        }
+
+        [TestMethod]
+        public void TestSurfboardType() 
+        {
+            // Arrange
+            var surfboard = new Surfboard { Type = "The Minilog" };
+
+            // Act
+            var result = surfboard.Type;
+
+            // Assert
+            Assert.AreEqual("The Minilog", result);
+        
+        }
     }
 }

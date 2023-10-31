8867b2304f6e539d8d3d7634f6f9b0be4f8f8765
Invenblocker <barthipost@gmail.com>
2023-10-31 11:13:37 +0100

    Fixed Rentee being an Int instead of a String.

--- SurfsProject/SurfsProject/Migrations/20231031101220_RenteeIsString.Designer.cs
+++ SurfsProject/SurfsProject/Migrations/20231031101220_RenteeIsString.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SurfsProject.Data;
 
@@ -11,9 +12,11 @@ using SurfsProject.Data;
 namespace SurfsProject.Migrations
 {
     [DbContext(typeof(SurfsProjectContext))]
-    partial class SurfsProjectContextModelSnapshot : ModelSnapshot
+    [Migration("20231031101220_RenteeIsString")]
+    partial class RenteeIsString
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
